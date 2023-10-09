125618a82ed93531da074b2529faa1b030e57c57
HammadHaider27 <haiderhammad251@gmail.com>
2023-10-09 08:28:15 -0700

    Fix some bugs

--- src/components/Peer.js
+++ src/components/Peer.js
@@ -1,4 +1,11 @@
-import { useVideo, useAVToggle } from "@100mslive/react-sdk";
+import {
+  useVideo,
+  useAVToggle,
+  useRemoteAVToggle,
+  useHMSActions,
+  selectLocalPeer,
+  useHMSStore,
+} from "@100mslive/react-sdk";
 import {
   FaMicrophone,
   FaMicrophoneSlash,
@@ -6,17 +13,25 @@ import {
   FaVideoSlash,
 } from "react-icons/fa";
 
-
 function Peer({ peer }) {
+  const hmsActions = useHMSActions();
+
   const { isLocalAudioEnabled, isLocalVideoEnabled, toggleAudio, toggleVideo } =
-  useAVToggle();
+    useAVToggle();
+  const localPeer = useHMSStore(selectLocalPeer);
+  const {
+    isAudioEnabled,
+    isVideoEnabled,
+    toggleAudio: remoteToggleAudio, // Destructuring
+    toggleVideo: remoteToggleVideo, // Destructuring
+  } = useRemoteAVToggle(peer.audioTrack, peer.videoTrack);
+
+  console.log("isAudioEnabled", isAudioEnabled);
 
   const { videoRef } = useVideo({
     trackId: peer.videoTrack,
   });
-  {
-    console.log(peer);
-  }
+  console.log(peer);
 
   return (
     <div className="peer-container">
@@ -27,24 +42,48 @@ function Peer({ peer }) {
         muted
         playsInline
       />
+
+      {/* ------------------------------- For Local peers ----------------------------------*/}
+
       <div className="peer-name">
-        {peer.name} {peer.roleName} {peer.isLocal ? "(You)" : ""} <br />
-
-        {(peer.isLocal || peer.roleName == "guest") && (
+        <b>{peer.roleName}</b> <br />
+        {peer.name} {peer.isLocal ? "(You)" : ""} <br />
+        {(peer.isLocal || peer.roleName === "guest") && (
           <div>
-            <>
-              <button onClick={toggleAudio}>
-                {isLocalAudioEnabled ? <FaMicrophone /> : <FaMicrophoneSlash />}
-              </button>
-              <button onClick={toggleVideo}>
-                {isLocalVideoEnabled ? <FaVideo /> : <FaVideoSlash />}
-              </button>
-            </>
-          </div>  
+            {peer.isLocal &&
+              (peer.roleName === "host" || peer.roleName === "guest") && (
+                <>
+                  <button onClick={toggleAudio}>
+                    {isLocalAudioEnabled ? (
+                      <FaMicrophone />
+                    ) : (
+                      <FaMicrophoneSlash />
+                    )}
+                  </button>
+                  <button onClick={toggleVideo}>
+                    {isLocalVideoEnabled ? <FaVideo /> : <FaVideoSlash />}
+                  </button>
+                </>
+              )}
+          </div>
         )}
-
-        {/* {peer.roleName === "host" || !peer.isLocal && <Footer />} */}
-        {/* <Footer /> */}
+        {/* --------------------------------- For Remote peers ----------------------------------*/}
+        {localPeer.roleName == "host" && !peer.isLocal ? (
+          <div>
+            <button
+              style={{ backgroundColor: "red" }}
+              onClick={remoteToggleAudio}
+            >
+              {isAudioEnabled ? <FaMicrophone /> : <FaMicrophoneSlash />}
+            </button>
+            <button
+              style={{ backgroundColor: "red" }}
+              onClick={remoteToggleVideo}
+            >
+              {isVideoEnabled ? <FaVideo /> : <FaVideoSlash />}
+            </button>
+          </div>
+        ) : null}
       </div>
     </div>
   );
